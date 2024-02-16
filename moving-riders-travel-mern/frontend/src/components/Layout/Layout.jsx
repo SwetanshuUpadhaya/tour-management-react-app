8583b91ba56e59beed920ecba30f88bd9ada8d91
Swetanshu Upadhaya <swetanshuupadhaya>
2024-02-16 21:56:38 +0530

    Initial commit for moving riders travel mern stack project.

---
+++ moving-riders-travel-mern/frontend/src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Header from '../Header/Header'
+import Router from '../../router/Router'
+import Footer from '../Footer/Footer'
+
+const layout = () => {
+  return (
+    <>
+      <Header />
+      <Router />
+      <Footer />
+    </>   
+  )
+}
+
+export default layout
