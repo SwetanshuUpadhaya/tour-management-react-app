ca377e0bdef32ed465ce98af161f7e027d9ec6fc
Swetanshu Upadhaya <swetanshuupadhaya>
2024-02-21 14:15:19 +0530

    Index file modified

--- moving-riders-travel-mern/frontend/src/components/Testimonial/Testimonials.jsx
+++ moving-riders-travel-mern/frontend/src/components/Testimonial/Testimonials.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row } from "reactstrap";
 import Slider from "react-slick";
 import ava01 from "../../assets/images/ava-1.jpg";
 import ava02 from "../../assets/images/ava-2.jpg";
@@ -36,54 +37,206 @@ const Testimonials = () => {
 
   return (
     <Slider {...settings}>
-      <div className="testimonial py-4 px-3">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste fugit
-          saepe quisquam rem dignissimos? Perferendis assumenda saepe neque
-          ipsum illo illum est obcaecati sit! Sit earum eligendi praesentium
-          quae distinctio!
-        </p>
+          <Row lg="3" className="d-flex align-items-center justify-content-center">
+            <div className="testimonial py-4 px-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                fugit saepe quisquam rem dignissimos? Perferendis assumenda
+                saepe neque ipsum illo illum est obcaecati sit! Sit earum
+                eligendi praesentium quae distinctio!
+              </p>
 
-        <div className="d-flex align-items-center gap-4 mt-3">
-          <img src={ava01} className="w-25 h-25 rounded-2" alt="" />
-          <div>
-            <h6 className="mb-0 mt-3">John Doe</h6>
-            <p>Customer</p>
-          </div>
-        </div>
-      </div>
-      <div className="testimonial py-4 px-3">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste fugit
-          saepe quisquam rem dignissimos? Perferendis assumenda saepe neque
-          ipsum illo illum est obcaecati sit! Sit earum eligendi praesentium
-          quae distinctio!
-        </p>
+              <div className="d-flex align-items-center gap-4 mt-3">
+                <img src={ava01} className="w-25 h-25 rounded-2" alt="" />
+                <div>
+                  <h6 className="mb-0 mt-3">John Doe</h6>
+                  <p>Customer</p>
+                </div>
+              </div>
+            </div>
+            <div className="testimonial py-4 px-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                fugit saepe quisquam rem dignissimos? Perferendis assumenda
+                saepe neque ipsum illo illum est obcaecati sit! Sit earum
+                eligendi praesentium quae distinctio!
+              </p>
 
-        <div className="d-flex align-items-center gap-4 mt-3">
-          <img src={ava02} className="w-25 h-25 rounded-2" alt="" />
-          <div>
-            <h6 className="mb-0 mt-3">John Doe</h6>
-            <p>Customer</p>
-          </div>
-        </div>
-      </div>
-      <div className="testimonial py-4 px-3">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste fugit
-          saepe quisquam rem dignissimos? Perferendis assumenda saepe neque
-          ipsum illo illum est obcaecati sit! Sit earum eligendi praesentium
-          quae distinctio!
-        </p>
+              <div className="d-flex align-items-center gap-4 mt-3">
+                <img src={ava02} className="w-25 h-25 rounded-2" alt="" />
+                <div>
+                  <h6 className="mb-0 mt-3">John Doe</h6>
+                  <p>Customer</p>
+                </div>
+              </div>
+            </div>
+            <div className="testimonial py-4 px-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                fugit saepe quisquam rem dignissimos? Perferendis assumenda
+                saepe neque ipsum illo illum est obcaecati sit! Sit earum
+                eligendi praesentium quae distinctio!
+              </p>
 
-        <div className="d-flex align-items-center gap-4 mt-3">
-          <img src={ava03} className="w-25 h-25 rounded-2" alt="" />
-          <div>
-            <h6 className="mb-0 mt-3">John Doe</h6>
-            <p>Customer</p>
-          </div>
-        </div>
-      </div>
+              <div className="d-flex align-items-center gap-4 mt-3">
+                <img src={ava03} className="w-25 h-25 rounded-2" alt="" />
+                <div>
+                  <h6 className="mb-0 mt-3">John Doe</h6>
+                  <p>Customer</p>
+                </div>
+              </div>
+            </div>
+          </Row>
+          <Row lg="3" className="d-flex align-items-center justify-content-center">
+            <div className="testimonial py-4 px-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                fugit saepe quisquam rem dignissimos? Perferendis assumenda
+                saepe neque ipsum illo illum est obcaecati sit! Sit earum
+                eligendi praesentium quae distinctio!
+              </p>
+
+              <div className="d-flex align-items-center gap-4 mt-3">
+                <img src={ava01} className="w-25 h-25 rounded-2" alt="" />
+                <div>
+                  <h6 className="mb-0 mt-3">John Doe</h6>
+                  <p>Customer</p>
+                </div>
+              </div>
+            </div>
+            <div className="testimonial py-4 px-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                fugit saepe quisquam rem dignissimos? Perferendis assumenda
+                saepe neque ipsum illo illum est obcaecati sit! Sit earum
+                eligendi praesentium quae distinctio!
+              </p>
+
+              <div className="d-flex align-items-center gap-4 mt-3">
+                <img src={ava02} className="w-25 h-25 rounded-2" alt="" />
+                <div>
+                  <h6 className="mb-0 mt-3">John Doe</h6>
+                  <p>Customer</p>
+                </div>
+              </div>
+            </div>
+            <div className="testimonial py-4 px-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                fugit saepe quisquam rem dignissimos? Perferendis assumenda
+                saepe neque ipsum illo illum est obcaecati sit! Sit earum
+                eligendi praesentium quae distinctio!
+              </p>
+
+              <div className="d-flex align-items-center gap-4 mt-3">
+                <img src={ava03} className="w-25 h-25 rounded-2" alt="" />
+                <div>
+                  <h6 className="mb-0 mt-3">John Doe</h6>
+                  <p>Customer</p>
+                </div>
+              </div>
+            </div>
+          </Row>
+          <Row lg="3" className="d-flex align-items-center justify-content-center">
+            <div className="testimonial py-4 px-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                fugit saepe quisquam rem dignissimos? Perferendis assumenda
+                saepe neque ipsum illo illum est obcaecati sit! Sit earum
+                eligendi praesentium quae distinctio!
+              </p>
+
+              <div className="d-flex align-items-center gap-4 mt-3">
+                <img src={ava01} className="w-25 h-25 rounded-2" alt="" />
+                <div>
+                  <h6 className="mb-0 mt-3">John Doe</h6>
+                  <p>Customer</p>
+                </div>
+              </div>
+            </div>
+            <div className="testimonial py-4 px-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                fugit saepe quisquam rem dignissimos? Perferendis assumenda
+                saepe neque ipsum illo illum est obcaecati sit! Sit earum
+                eligendi praesentium quae distinctio!
+              </p>
+
+              <div className="d-flex align-items-center gap-4 mt-3">
+                <img src={ava02} className="w-25 h-25 rounded-2" alt="" />
+                <div>
+                  <h6 className="mb-0 mt-3">John Doe</h6>
+                  <p>Customer</p>
+                </div>
+              </div>
+            </div>
+            <div className="testimonial py-4 px-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                fugit saepe quisquam rem dignissimos? Perferendis assumenda
+                saepe neque ipsum illo illum est obcaecati sit! Sit earum
+                eligendi praesentium quae distinctio!
+              </p>
+
+              <div className="d-flex align-items-center gap-4 mt-3">
+                <img src={ava03} className="w-25 h-25 rounded-2" alt="" />
+                <div>
+                  <h6 className="mb-0 mt-3">John Doe</h6>
+                  <p>Customer</p>
+                </div>
+              </div>
+            </div>
+          </Row>
+          <Row lg="3" className="d-flex align-items-center justify-content-center">
+            <div className="testimonial py-4 px-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                fugit saepe quisquam rem dignissimos? Perferendis assumenda
+                saepe neque ipsum illo illum est obcaecati sit! Sit earum
+                eligendi praesentium quae distinctio!
+              </p>
+
+              <div className="d-flex align-items-center gap-4 mt-3">
+                <img src={ava01} className="w-25 h-25 rounded-2" alt="" />
+                <div>
+                  <h6 className="mb-0 mt-3">John Doe</h6>
+                  <p>Customer</p>
+                </div>
+              </div>
+            </div>
+            <div className="testimonial py-4 px-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                fugit saepe quisquam rem dignissimos? Perferendis assumenda
+                saepe neque ipsum illo illum est obcaecati sit! Sit earum
+                eligendi praesentium quae distinctio!
+              </p>
+
+              <div className="d-flex align-items-center gap-4 mt-3">
+                <img src={ava02} className="w-25 h-25 rounded-2" alt="" />
+                <div>
+                  <h6 className="mb-0 mt-3">John Doe</h6>
+                  <p>Customer</p>
+                </div>
+              </div>
+            </div>
+            <div className="testimonial py-4 px-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                fugit saepe quisquam rem dignissimos? Perferendis assumenda
+                saepe neque ipsum illo illum est obcaecati sit! Sit earum
+                eligendi praesentium quae distinctio!
+              </p>
+
+              <div className="d-flex align-items-center gap-4 mt-3">
+                <img src={ava03} className="w-25 h-25 rounded-2" alt="" />
+                <div>
+                  <h6 className="mb-0 mt-3">John Doe</h6>
+                  <p>Customer</p>
+                </div>
+              </div>
+            </div>
+          </Row>
     </Slider>
   );
 };
