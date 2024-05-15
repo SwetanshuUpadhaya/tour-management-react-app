f3b92fd0dda5fd504f8cf753c558bab51d5f986f
Swetanshu Upadhaya <swetanshuupadhaya@gmail.com>
2024-05-15 16:28:05 +0530

    .

--- moving-riders-travel-mern/backend/index.js
+++ moving-riders-travel-mern/backend/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions = {
-  origin: true,
+  origin: ["https://moving-riders-travel-server.vercel.app"],
   credentials: true,
 };
 app.use(express.static("dist"));
